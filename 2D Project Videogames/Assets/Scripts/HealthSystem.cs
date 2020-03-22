7e802e0780d3f23377956d3811995c9ece74512f
Richie_Sjt <richi252012@hotmail.com>
2020-03-22 14:49:51 -0600

    Player movement, smooth camera

---
+++ 2D Project Videogames/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class HealthSystem{
+    private int health;
+    private int maxHealth;
+    public event EventHandler OnHealthChanged;
+
+    public HealthSystem(int maxHealth) {
+        this.maxHealth = maxHealth;
+        health = maxHealth;
+    }
+
+    public int GetHealth() {
+        return health;
+    }
+
+    public float GetHealthPercent() {
+        return (float) health / maxHealth; 
+    }
+
+    public void TakeDamage(int damage) {
+        health -= damage;
+
+        OnHealthChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void Heal(int healAmount) {
+        health += healAmount;
+        if (health > maxHealth)
+            health = maxHealth;
+
+        OnHealthChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
--- 2D Project Videogames/Assets/Scripts/Player.cs
+++ 2D Project Videogames/Assets/Scripts/Player.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour{
-    
+
+    public float speed = 0.12f;
+    private HealthSystem healthSystem;
+
     void Start(){
-        
+        healthSystem = new HealthSystem(100);
     }
 
     void Update(){
-        
+        float x = Input.GetAxis("Horizontal") * speed;
+        float y = Input.GetAxis("Vertical") * speed;
+
+        gameObject.transform.Translate(new Vector3(x, y, 0));
     }
-    
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.tag == "Enemy") {
+            
+            //Meteor meteor = collision.GetComponent<Meteor>();
+            //healthSystem.TakeDamage(meteor.GetDamage());
+            if (healthSystem.GetHealth() <= 0)
+                Destroy(gameObject); 
+        }
+    }
     
 }
