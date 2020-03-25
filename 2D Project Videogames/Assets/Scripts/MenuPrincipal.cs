3fed6fe532618c0f71f9adfee2f6d6ce57a9e002
Eduardo Herrera <lalo2000x@hotmail.com>
2020-03-25 16:17:08 -0600

    Menu Principal

---
+++ 2D Project Videogames/Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EmpezarJuego(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void Salir(){
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+}
