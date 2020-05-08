b021705c9b36df7eb2d66b37395c422da98b06a7
Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>
2020-05-07 23:31:18 -0500

    Demon hitbox

--- 2D Project Videogames/Assets/Scripts/Controllers/DemonBossController.cs
+++ 2D Project Videogames/Assets/Scripts/Controllers/DemonBossController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class DemonBossController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Animator")]    
+    public Animator anim;
+    public float attackDuration = 0.3f;
+    
+    [Header("Health")]
+    private HealthSystem healthSystem;
+    public int maxHealth = 100;
+
+    [Header("Attack Settings")]
+    public GameObject hitBox;
+    public int attackDamage = 10;
+    public float attackRate = 5f;
+    private float nextAttackTime = 0f;
+    public float lookRadius = 5f;
+
+    private float currentTime = 0f;
+
+
     void Start()
     {
-        
+        healthSystem = GetComponent<HealthSystem>();
+        healthSystem.maxHealth = maxHealth;
+        healthSystem.health = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
