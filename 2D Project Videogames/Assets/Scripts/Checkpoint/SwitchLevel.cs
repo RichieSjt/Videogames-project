00e1eb374b2a13164003fd525bea5d478f0bf713
Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>
2020-05-27 10:43:21 -0500

    Scene switch

---
+++ 2D Project Videogames/Assets/Scripts/Checkpoint/SwitchLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevel : MonoBehaviour
+{
+    public int sceneIndex = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneIndex);
+            Debug.Log("NEW SCENE");
+        }
+    }
+}
