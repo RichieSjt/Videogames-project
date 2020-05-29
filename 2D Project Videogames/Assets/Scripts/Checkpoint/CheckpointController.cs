4fef77a6b41c4c6d56df0686ac0ab6bf5a3bc110
Richie_Sjt <richi252012@hotmail.com>
2020-05-29 15:55:48 -0500

    Graveyard boundaries and items save/load
    Co-Authored-By: Pedro Rangel <pedrorangelp@users.noreply.github.com>

--- 2D Project Videogames/Assets/Scripts/Checkpoint/CheckpointController.cs
+++ 2D Project Videogames/Assets/Scripts/Checkpoint/CheckpointController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Script attached to checkpoints
 public class CheckpointController : MonoBehaviour
 {
     private Vector3 checkpoint;
     private bool checkedpoint = false;
-
+    
     private void Update()
     {
         if(checkedpoint)
@@ -20,6 +21,7 @@ public class CheckpointController : MonoBehaviour
             checkpoint = other.transform.position;
             other.GetComponent<CheckpointDetection>().SetLastCheckpoint(checkpoint);
             checkedpoint = true;
+            gameObject.GetComponent<ProgressManager>().Save();
             //SceneManager.GetActiveScene().buildIndex;
         }
     }
--- 2D Project Videogames/Assets/Scripts/Checkpoint/SwitchLevel.cs
+++ 2D Project Videogames/Assets/Scripts/Checkpoint/SwitchLevel.cs
@@ -19,40 +19,81 @@ public class SwitchLevel : MonoBehaviour
     public int sceneIndex = 0;
 
 
+    private void Start() {
+        SetFixedPlayerPosition();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            //Scene sceneToLoad = SceneManager.GetSceneByBuildIndex(sceneIndex);
-            SceneManager.LoadScene(sceneIndex);
-            //SceneManager.MoveGameObjectToScene(other.gameObject, sceneToLoad);
-            Debug.Log("NEW SCENE");
+            Load();
+            Invoke("Save", 0.5f);
+            Invoke("ChangeScene", 1f);
         }
     }
 
     private void SetFixedPlayerPosition()
     {
+        //Forest level
         if (sceneIndex == 1)
         {
-
+            playerPosition = new Vector3(-72f,-0.33f,0);
         }
+        //Village level
         if (sceneIndex == 2)
         {
             playerPosition = new Vector3(-15.88f,-0.3299999f,0);
         }
+        //Castle level
         if (sceneIndex == 3)
         {
-
+            playerPosition = new Vector3(-21f,-0.33f,0);
         }
+        //Boss level
+        //Graveyard level
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 
     #region ProgressManager
+    public void Save()
+    {
+        SaveObject saveObject = new SaveObject {
+            playerPosition = this.playerPosition,
+            playerHealth = this.playerHealth,
+            savedCurrentMagic = this.savedCurrentMagic,
+            savedMagicAttacks = this.savedMagicAttacks,
+            InventoryList = this.InventoryList,
+            currentScene = sceneIndex,
+        };
+        string json = JsonUtility.ToJson(saveObject);
+
+        File.WriteAllText(Application.dataPath+"/save.txt",json);
+
+        /*#region DebugSaveObject
+        Debug.Log("FILE SUCCESSFULLY SAVED");
+        Debug.Log("Player position: " + saveObject.playerPosition);
+        Debug.Log("Player health: " + saveObject.playerHealth);
+        Debug.Log("Magic attacks: " + saveObject.savedMagicAttacks);
+        Debug.Log("Current magic: " + saveObject.savedCurrentMagic);
+        Debug.Log("Items: " + saveObject.InventoryList);
+        Debug.Log("Scene index: " + saveObject.currentScene);
+        #endregion*/
+    }
     public void Load()
     {
         if (System.IO.File.Exists(Application.dataPath+"/save.txt")){
             string saveString = File.ReadAllText(Application.dataPath+"/save.txt");
             SaveObject saveObject = JsonUtility.FromJson<SaveObject>(saveString);
 
+            playerHealth = saveObject.playerHealth;
+            savedCurrentMagic = saveObject.savedCurrentMagic;
+            savedMagicAttacks = saveObject.savedMagicAttacks;
+            InventoryList = saveObject.InventoryList;
         }
     }
     #endregion
